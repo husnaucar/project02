aa072382f06cb7ae6548c4e107a681d0981821e8
Husna <ucarisikhusna@gmail.com>
2020-11-08 20:08:06 +0300

    Forth commit from local master

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
 		System.out.println("First commit from remote master branch");
 		System.out.println("Second commit from remote master branch");
 		System.out.println("Third commit from remote master branch");
-
+	System.out.println("Here is a merge conflict I create from local master");
 	}
 
 }
