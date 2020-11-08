ff4e2fd848a3656620a23dd37ade3847e526a247
Husna <ucarisikhusna@gmail.com>
2020-11-08 19:09:12 +0300

    second commit form local master

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local master branch");
+		System.out.println("Second commit from local master");
 
 	}
 
